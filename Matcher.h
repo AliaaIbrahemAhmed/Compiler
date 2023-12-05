f6c71f25607d93ac2233849163644423b09e9d9b
AliaaFathy <aliaaf518@gmail.com>
2023-12-05 03:32:23 +0200

    initial

---
+++ Matcher.h
@@ -0,0 +1,45 @@
+#ifndef MATCHER_H
+#define MATCHER_H
+
+#include <iostream>
+#include <fstream>
+#include"State.h"
+#include <string>
+#include <vector>
+#include <map>
+#include <set>
+#include "NFATODFA.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Matcher
+{
+public:
+    Matcher();
+    virtual ~Matcher();
+
+    vector<string> get_sym_table();
+
+    void match(vector<string> token,pair<TRANSITION_TABLE, Node > DFA);
+
+    void set_output_file_name(string output_file_name);
+
+protected:
+
+private:
+
+    vector<string> symbol_table;
+    string output_file_name;
+    string out;
+
+    void write_output_file(string name);
+
+    bool error_recovery(string str, pair<TRANSITION_TABLE, Node > DFA);
+
+    bool fun(string str, pair<TRANSITION_TABLE, Node > DFA);
+
+};
+
+#endif // MATCHER_H
